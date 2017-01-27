43415f1ec9f4202d51def93c7e5b7ae93f1067be
Erica Nafziger <garbererica@gmail.com>
2017-01-27 11:28:03 -0800

    add more categories to filter by with filter pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -15,6 +15,27 @@ export class FilterPipe implements PipeTransform {
           }
         }
         return output;
+      } else if (filter === "maleCharacters") {
+        for (var i=0; i <input.length; i++) {
+          if (input[i].gender === "Male") {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else if (filter === "humanCharacters"){
+        for (var i=0; i <input.length; i++) {
+          if (input[i].species === "Human") {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else if (filter === "faeCharacters") {
+        for (var i=0; i <input.length; i++) {
+          if (input[i].species === "Fae") {
+            output.push(input[i]);
+          }
+        }
+        return output;
       } else {
         return input;
       }
