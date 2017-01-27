0b9ac2e1d75224ccaa240204c6e13138de55675c
Erica Nafziger <garbererica@gmail.com>
2017-01-27 15:48:31 -0800

    remove api keys

--- src/app/api-keys.ts
+++
@@ -1,7 +0,0 @@
-export var masterFirebaseConfig = {
-  apiKey: "AIzaSyC_ZRHuFGrde9x_elfxToj383t7HffhYvc",
-  authDomain: "character-list.firebaseapp.com",
-  databaseURL: "https://character-list.firebaseio.com",
-  storageBucket: "character-list.appspot.com",
-  messagingSenderId: "879262967268"
-};
